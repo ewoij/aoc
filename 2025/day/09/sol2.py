295e3cb0357693e65fc1eb89e80d00ed5dfa186a
Jonas Berdoz <jonas.berdoz@gmail.com>
2025-12-09 12:08:34 +0100

    ...

--- 2025/day/09/sol2.py
+++ 2025/day/09/sol2.py
@@ -15,6 +15,7 @@ def binary_to_green_image(arr):
 
     # green channel = 255 when arr == 1
     rgb[arr == 1] = [0, 255, 0]
+    rgb[arr == 2] = [255, 0, 0]
 
     return Image.fromarray(rgb)
 
@@ -36,8 +37,10 @@ def area(a, b):
     return reduce(mul, [abs(a-b)+1 for a, b in zip(a,b)])
 
 tiles = np.array(tiles)
+tiles_original = tiles.copy()
 
 tiles = np.concatenate([tiles[:, [1]], tiles[:, [0]]], axis=1)
+tiles = (tiles / 200).astype(int)
 
 tiles[:, 0] = tiles[:, 0] - tiles[:, 0].min()
 tiles[:, 1] = tiles[:, 1] - tiles[:, 1].min()
@@ -55,7 +58,7 @@ for i in range(tiles.shape[0]):
     M[r0:r1+1, c0:c1+1] = 1
 
 # color
-stack = [(M.shape[0] // 2, M.shape[1] // 2)]
+stack = [(M.shape[0] // 3, M.shape[1] // 2)]
 while stack:
     r, c = stack.pop()
     M[r, c] = 1
@@ -64,7 +67,31 @@ while stack:
             if M[r_, c_] == 0:
                 stack.append((r_, c_))
 
-# for r in range(M.shape[0]):
-#     print(''.join('X' if M[r, c] else ' ' for c in range(M.shape[1])))
 
-# binary_to_green_image(M).save('test.png')
+# stuff
+max_ = 0
+max_pair = None
+
+for i in range(tiles.shape[0]):
+    print(i)
+    for j in range(i+1, tiles.shape[0]):
+        a = tiles[i, :]
+        b = tiles[j, :]
+        r0, r1 = sorted([a[0], b[0]])
+        c0, c1 = sorted([a[1], b[1]])
+        if list(np.unique(M[r0:r1+1, c0:c1+1])) == [1]:
+            area_ = area(a, b)
+            if area_ > max_:
+                max_ = area_
+                max_pair = (i, j)
+
+a, b = max_pair
+print(area(tiles_original[a, :], tiles_original[b, :]))
+
+a, b = tiles[a, :], tiles[b, :]
+
+r0, r1 = sorted([a[0], b[0]])
+c0, c1 = sorted([a[1], b[1]])
+M[r0:r1+1, c0:c1+1] = 2
+
+binary_to_green_image(M).save('test.png')
