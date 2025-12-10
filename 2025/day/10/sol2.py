7d0a3308b6fe1eb39c5f44a1d06e21291ac50fbf
Jonas Berdoz <jonas.berdoz@gmail.com>
2025-12-10 20:30:18 +0100

    use pulp solver

--- 2025/day/10/sol2.py
+++ 2025/day/10/sol2.py
@@ -1,5 +1,5 @@
-from functools import cache
 from dataclasses import dataclass
+import pulp as pl
 
 txt = """
 [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
@@ -24,49 +24,25 @@ for line in txt.strip().split('\n'):
     ))
 
 
-def toto(machine: Machine):
-    buttons = machine.buttons
-    buttons = sorted(buttons, key=lambda v: -len(v))
-    target = tuple(machine.target)
+def solve(machine: Machine):
+    prob = pl.LpProblem('toto', pl.LpMinimize)
 
-    def next_(target, button):
-        target = list(target)
-        for i in button:
-            target[i] -= 1
-        return tuple(target)
+    vars = [pl.LpVariable(str(j), lowBound=0, cat=pl.LpInteger) for j in range(len(machine.buttons))]
 
-    def find_max(target, button):
-        assert all(v >= 0 for v in target), 'invalid state'
-        c = 0
-        while not any(v < 0 for v in target):
-            target = next_(target, button)
-            c += 1
-        return c - 1
+    for i in range(len(machine.target)):
+        vars_ = [vars[j] for j, v in enumerate(machine.buttons) if i in v]
+        prob += sum(vars_) == machine.target[i], f'sum {i}'
 
-    def backtrack(idx, target):
-        if idx >= len(buttons): 
-            if all(v == 0 for v in target):
-                return []
-            else:
-                return None
-        max_ = find_max(target, buttons[idx])
-        for i in reversed(range(max_+1)):
-            n_target = target
-            for _ in range(i):
-                n_target = next_(n_target, buttons[idx])
-            res = backtrack(idx+1, n_target)
-            if res is not None: return [i] + res
-        return None
+    prob += sum(vars), 'minimize sum'
 
-    return backtrack(0, target)
+    prob.solve()
 
+    return [v.varValue for v in vars]
 
-toto(machines[4])
+res = 0
+for m in machines:
+    r = sum(solve(m))
+    print(r)
+    res += r
 
-# res = 0
-# for m in machines:
-#     r = toto(m)
-#     print(sum(r), r)
-#     res += sum(r)
-# 
-# print(res)
+print(res)
